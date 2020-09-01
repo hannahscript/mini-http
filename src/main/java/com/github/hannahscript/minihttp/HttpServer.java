6977c69cca392c44d1f3f8097d34764485674baa
hannahscript <h4nn4h777@protonmail.com>
2020-09-01 17:36:47 +0200

    Act as an echo server

--- src/main/java/com/github/hannahscript/minihttp/HttpServer.java
+++ src/main/java/com/github/hannahscript/minihttp/HttpServer.java
@@ -3,6 +3,7 @@ package com.github.hannahscript.minihttp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,6 +13,7 @@ public class HttpServer {
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private BufferedReader clientReader;
+    private PrintWriter clientWriter;
 
     public HttpServer(int port) {
         this.port = port;
@@ -21,13 +23,21 @@ public class HttpServer {
         this.serverSocket = new ServerSocket(this.port);
 
         this.clientSocket = this.serverSocket.accept();
-        this.clientReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        this.clientReader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+        this.clientWriter = new PrintWriter(this.clientSocket.getOutputStream(), true);
 
-        clientReader.readLine();
+
+        this.clientWriter.println("Connected to mini-http");
+        String clientMessage;
+        do {
+            clientMessage = this.clientReader.readLine();
+            clientWriter.println("You sent '" + clientMessage + "'");
+        } while(!clientMessage.equals(""));
     }
 
     public void stop() throws IOException {
         this.clientReader.close();
+        this.clientWriter.close();
         this.clientSocket.close();
         this.serverSocket.close();
     }
